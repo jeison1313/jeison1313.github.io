a93ca117840757f7352bb2cb5aa0f1851fdf83c9
jeison <aliceshlendo13@gmail.com>
2019-11-17 18:21:59 -0400

    termine lo establecido en el tutorial y ademos agregue una funcion de sombreado al pasar por las tareass y poder agregar una tarea dando enter

--- js/main.js
+++ js/main.js
@@ -6,20 +6,52 @@
 
    // funciones
 var agregarTarea = function() {
-  alert("agregar");
+    var tarea = tareaInput.value,
+        nuevaTarea = document.createElement("li"),
+        enlace = document.createElement("a"),
+        contenido = document.createTextNode(tarea);
+
+        if (tarea === "") {
+          tareaInput.setAttribute("placeholder", "agrega una tarea validad");
+          tareaInput.className = "error";
+          return false;
+        }
+     // agregamos el contenido al enlace
+      enlace.appendChild(contenido);
+    // le establecemos un atributo href
+      enlace.setAttribute("href", "#");
+    // agregamos el enlace a la nuevatarea
+      nuevaTarea.appendChild(enlace);
+    // agregamos la nueva tarea a la Lista
+      lista.appendChild(nuevaTarea);
+
+      tareaInput.value = "";
+
+      for (var i = 0; i <= lista.children.length -1; i++) {
+        lista.children[i].addEventListener("click", function(){
+          this.parentNode.removeChild(this);
+        });
+      }
+
 };
 var comprobarInput = function() {
-  alert("comprobar");
+   tareaInput.className = "";
+   tareaInput.setAttribute("placeholder", "Agrega tu tarea");
 };
 
 var eliminarTarea = function() {
-  alert("eliminar");
+  this.parentNode.removeChild(this);
 };
 
    // eventos
 
   // Agragar Tarea
    btnNuevaTarea.addEventListener("click", agregarTarea);
+   tareaInput.addEventListener("keyup", function(event){
+     if (event.keyCode === 13) {
+       agregarTarea();
+     }
+   });
 
   // Comprobar input
   tareaInput.addEventListener("click", comprobarInput);
